effc37b4d73ce09186ec21a250952b6b14dd91f5
Daniel Tefera <dayuzuel@gmail.com>
2025-11-09 10:17:52 -0800

    added buttons and check reegistration

--- bot/main.py
+++ bot/main.py
@@ -6,19 +6,38 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 from telegram.request import HTTPXRequest
 from bot.handlers.register import register_handler
+from telegram import ReplyKeyboardMarkup
+from  bot.utils.db import load_data
 
 load_dotenv()
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
+async def check(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    data = load_data()
+    user_name = user.first_name
+    found_entries = [entry for entry in data if entry["name"] == user_name]
+    if found_entries: 
+        entries_text = "".join([f'Name: {e[name]}, Phone: {e[phone]}' for e in found_entries])
+        await update.message.reply_text(f"You are registered: {entries_text}")
+    else:
+        await update.message.reply_text("You are not registered yet. Use /regiser to register")
+
+    
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [["/reister", "/check"]]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
     await update.message.reply_text(
-        "👋 Welcome to the Registration Bot!\n\nUse /register to Register or /help for Guidance."
+        "👋 Welcome to the Registration Bot!\n\nUse the buttons below to register or check your info", reply_markup=reply_markup
     )
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [["/reister", "/check"]]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
     await update.message.reply_text(
         "Welcome! This is a simple registration bot!\n"
-        "Use the bot with these commands:\n"
+        "Use the buttons or these commands:\n"
         "/start - Restart the bot.\n"
         "/register - Enter your information and join.\n"
         "/check - Check your information if you have registred.\n "
@@ -31,6 +50,7 @@ def main():
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("check", check))
     app.add_handler(register_handler)
 
 
